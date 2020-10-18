b2ad93554adbd4fb7d5007580358dcd961872620
Nitish Kumar <niralanitrogensir@gmail.com>
2020-10-18 15:29:08 +0530

    this is Second commit , Android Udacity Learning. in this commit i am working with array.

--- app/src/main/java/com/example/androidlearn/NumbersActivity.java
+++ app/src/main/java/com/example/androidlearn/NumbersActivity.java
@@ -2,6 +2,7 @@ package com.example.androidlearn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
 import android.view.View;
 
 import android.os.Bundle;
@@ -13,5 +14,20 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        String []words = new String[10];
+        words[0] = "One";
+        words[1] = "Two";
+        words[2] = "Three";
+        words[3] = "Four";
+        words[4] = "Five";
+        words[5] = "Six";
+        words[6] = "Seven";
+        words[7] = "Eight";
+        words[8] = "Nine";
+        words[9] = "Ten";
+        Log.v("NumbersActivity","Words at Index 0 :" + words[0]);
+        Log.v("NumbersActivity","Words at Index 1 :" + words[1]);
+
     }
 }
