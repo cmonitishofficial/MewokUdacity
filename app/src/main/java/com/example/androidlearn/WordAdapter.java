6c55f13c871277895c6360e76976b23f9732762a
Nitish Kumar <niralanitrogensir@gmail.com>
2020-10-22 12:50:34 +0530

    This is My Fourth commit , In this Commit i have learn about ListView and custumArrayAdapter which controls scrollview and also use very less memory as compare to LinearLayout with vertical Orientation . also learn about create custom class and custom Araray Adapter.

--- app/src/main/java/com/example/androidlearn/WordAdapter.java
+++ app/src/main/java/com/example/androidlearn/WordAdapter.java
@@ -1,16 +1,57 @@
 package com.example.androidlearn;
 
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WordAdapter extends ArrayAdapter<word> {
-    @NonNull
+
+    public WordAdapter(Activity context, ArrayList<word>Words) {
+        super(context, 0, Words);
+    }
+
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Check if the existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.listitem, parent, false);
+        }
+
+        // Get the {@link AndroidFlavor} object located at this position in the list
+        word currentWord = getItem(position);
+
+        // Find the TextView in the list_item.xml layout with the ID version_name
+        TextView miwok_textview = (TextView) listItemView.findViewById(R.id.miwok_textview);
+        // Get the version name from the current AndroidFlavor object and
+        // set this text on the name TextView
+        miwok_textview.setText(currentWord.getmMiwokTranslation());
+
+        // Find the TextView in the list_item.xml layout with the ID version_number
+        TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+        // Get the version number from the current AndroidFlavor object and
+        // set this text on the number TextView
+        defaultTextView.setText(currentWord.getmDefaultTranslation());
+
+//        // Find the ImageView in the list_item.xml layout with the ID list_item_icon
+//        ImageView iconView = (ImageView) listItemView.findViewById(R.id.list_item_icon);
+//        // Get the image resource ID from the current AndroidFlavor object and
+//        // set the image to iconView
+//        iconView.setImageResource(currentAndroidFlavor.getImageResourceId());
+//
+//        // Return the whole list item layout (containing 2 TextViews and an ImageView)
+//        // so that it can be shown in the ListView
+        return listItemView;
     }
 }
